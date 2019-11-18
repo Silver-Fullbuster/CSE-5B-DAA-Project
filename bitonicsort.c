9c7a47cae2ead482c46e9908df3275f0ff2e1b70
Kartikeya Chauhan <kc1.408596@gmail.com>
2019-11-18 16:29:00 +0530

    formatting

--- bitonicsort.c
+++ bitonicsort.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #define n 8
 
 int up = 1, down = 0, a[n] = {9,8,7,6,1,2,4,3};
 
-void bitonic_compare(int i, int j, int dir)
-{
+void bitonic_compare(int i, int j, int dir){
     int temp;
- 
-    if (dir == (a[i] > a[j]))
-    {
+    if (dir == (a[i] > a[j])){
         temp = a[i];
         a[i] = a[j];
         a[j] = temp;
     }
 }
 
-void bitonic_merge(int low, int high, int dir)
-{
+void bitonic_merge(int low, int high, int dir){
     int mid, i;
- 
-    if (high > 1)
-    {
+    if (high > 1){
         mid = high / 2;
         for (i = low; i < (low + mid); i++)
             bitonic_compare(i, i + mid, dir);    
@@ -31,19 +24,15 @@ void bitonic_merge(int low, int high, int dir)
     }
 }
 
-void bitonic_sort(int low, int high, int dir)
-{
+void bitonic_sort(int low, int high, int dir){
     int mid;
- 
-    if (high > 1)
-    {
+    if (high > 1){
         mid = high / 2;
         bitonic_sort(low, mid, up);
         bitonic_sort(low + mid, mid, down);
         bitonic_merge(low, high, dir);
     }
 }
-
 
 int main(){
 	int i;
@@ -53,4 +42,3 @@ int main(){
 		printf("%d",a[i]);
 	return 0;
 }
-
